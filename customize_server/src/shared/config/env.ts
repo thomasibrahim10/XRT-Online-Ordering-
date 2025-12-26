6d62871cf0ebd2dfcc141f93fd4cc74666512e65
Zaroscript <andrewazer18@gmail.com>
2025-12-26 12:57:48 +0200

    refactor: Switch server module system from ESNext/ESM to CommonJS.

--- customize_server/src/shared/config/env.ts
+++ customize_server/src/shared/config/env.ts
@@ -1,9 +1,5 @@
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
 import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
